68467fa17348cb57b9d58c0d17cb71dfe548bfb8
pjy4183 <pjy4183@gmail.com>
2020-07-14 21:32:30 -0500

    update save button

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from pybo.models import Question
 from ..forms import QuestionForm
+from datetime import datetime
+from werkzeug.utils import redirect
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -21,4 +23,10 @@ def detail(question_id):
 @bp.route('/create/',methods=('GET', 'POST'))
 def create():
     form = QuestionForm()
+
+    if request.form = 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
