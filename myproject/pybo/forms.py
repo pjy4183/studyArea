578a2b00af07cba27b6cb480c71f78860cba1d0e
pjy4183 <pjy4183@gmail.com>
2020-07-21 00:08:24 -0500

    flask-user_account

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -8,3 +8,9 @@ class QuestionForm(FlaskForm):
 
 class AnswerForm(FlaskForm):
     content = TextAreaField('내용', validators=[DataRequired('내용은 필수 입력 항목입니다')])
+
+class UserCreateForm(FLaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(),Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
+    email = EmailField('이메일', [DataRequired(), Email()])
