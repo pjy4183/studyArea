d8ebf21109ec0e7b59c622f21fe392384c3a68ac
pjy4183 <pjy4183@gmail.com>
2020-07-22 23:46:19 -0500

    Update login & logout

--- myproject/pybo/views/auth_views.py
+++ myproject/pybo/views/auth_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, flash, request, session
+from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
@@ -29,9 +29,9 @@ def login():
     form = UserLoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         error = None
-        user = User.query.filter_by(username-form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if not user:
-            error = "존재하지 않는 계정입니다."
+            error = "존재하지 않는 사용자입니다."
         elif not check_password_hash(user.password, form.password.data):
             error = "비밀번호가 올바르지 않습니다."
         if error is None:
@@ -40,3 +40,16 @@ def login():
             return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
